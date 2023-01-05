7970fdd453eda27bcf88704681144dd98c29c5be
Filopatir-Mamdouh <Filopatirmamdouh@gmail.com>
2023-01-05 13:48:55 +0200

    design body content scroll and container

--- lib/Flutter.dart
+++ lib/Flutter.dart
@@ -13,26 +13,44 @@ class _LearnFlutterState extends State<LearnFlutter> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Learn Flutter'),
-        automaticallyImplyLeading: false,
-        leading: IconButton(
-          onPressed: () {
-            Navigator.of(context).pop();
-          },
-          icon: Icon(Icons.arrow_back_ios),
-        ),
-        actions: [
-          IconButton(
+        appBar: AppBar(
+          title: const Text('Learn Flutter'),
+          automaticallyImplyLeading: false,
+          leading: IconButton(
             onPressed: () {
-              debugPrint('Actions');
+              Navigator.of(context).pop();
             },
-            icon: const Icon(
-              Icons.info_outline,
-            ),
+            icon: Icon(Icons.arrow_back_ios),
           ),
-        ],
-      ),
-    );
+          actions: [
+            IconButton(
+              onPressed: () {
+                debugPrint('Actions');
+              },
+              icon: const Icon(
+                Icons.info_outline,
+              ),
+            ),
+          ],
+        ),
+        body: SingleChildScrollView(
+            child: Column(children: [
+          Image.asset('assets/images.jpg'),
+          const SizedBox(
+            height: 10,
+            width: 50,
+          ),
+          const Divider(
+            color: Colors.black,
+          ),
+          Container(
+              margin: const EdgeInsets.all(10.0),
+              padding: const EdgeInsets.all(10.0),
+              color: Colors.lightGreen,
+              child: const Center(
+                child: const Text('this is text ',
+                    style: TextStyle(color: Colors.green)),
+              )),
+        ])));
   }
 }
