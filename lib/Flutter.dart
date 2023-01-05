a8c940cf427bdddbc17a19289c1ecac766de8fd9
Filopatir-Mamdouh <Filopatirmamdouh@gmail.com>
2023-01-05 13:40:18 +0200

    create app bar content icon button

---
+++ lib/Flutter.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class LearnFlutter extends StatefulWidget {
+  const LearnFlutter({super.key});
+
+  @override
+  State<LearnFlutter> createState() => _LearnFlutterState();
+}
+
+class _LearnFlutterState extends State<LearnFlutter> {
+  bool isSwitch = false;
+  bool? ischeck = false;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Learn Flutter'),
+        automaticallyImplyLeading: false,
+        leading: IconButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          icon: Icon(Icons.arrow_back_ios),
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {
+              debugPrint('Actions');
+            },
+            icon: const Icon(
+              Icons.info_outline,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
